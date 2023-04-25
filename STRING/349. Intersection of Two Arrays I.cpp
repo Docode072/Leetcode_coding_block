443e78f1ac2a04c84a2bcefb3b8d0c9e65da2c7f
Himanshu Singh <120442022+Docode072@users.noreply.github.com>
2023-04-25 20:23:59 +0530

    Create 349. Intersection of Two Arrays I.cpp
    349. Intersection of Two Arrays
    Easy
    4.6K
    2.1K
    Companies
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [9,4]
    Explanation: [4,9] is also accepted.
     
    
    Constraints:
    
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 1000
    
    https://leetcode.com/problems/intersection-of-two-arrays/description/

---
+++ STRING/349. Intersection of Two Arrays I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+         vector<int>ans;
+        unordered_map<int , int> m1;
+
+        for(auto x : nums1) m1[x]++;
+        for(int i = 0 ; i< nums2.size(); i++){
+            if(m1[nums2[i]]){
+                ans.push_back(nums2[i]);
+                m1[nums2[i]] = 0;
+            }
+        }
+        return ans;
+    }
+};
